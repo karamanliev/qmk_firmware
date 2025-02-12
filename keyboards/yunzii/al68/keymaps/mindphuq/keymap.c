b4522a93063224128592c4ec51278957713cb841
Hristo Karamanliev <karamanliev@gmail.com>
2025-02-12 16:07:16 +0200

    add some goodies

--- keyboards/yunzii/al68/keymaps/mindphuq/keymap.c
+++ keyboards/yunzii/al68/keymaps/mindphuq/keymap.c
@@ -15,6 +15,37 @@
  */
 #include QMK_KEYBOARD_H
 
+#define CTL_ESC CTL_T(KC_ESC)
+enum user_keycodes {
+    OS_CHG = SAFE_RANGE,
+    PRT_EM,
+    PSCR,
+    DEL_WRD,
+    MUL_PST
+};
+
+// Call the post init code.
+void keyboard_post_init_user(void) {
+    rgb_matrix_enable_noeeprom();
+    rgb_matrix_sethsv_noeeprom(191, 200, 120);
+    rgb_matrix_mode_noeeprom(RGB_MATRIX_SOLID_COLOR);
+}
+
+// Custom keycodes setup
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    if (!record->event.pressed) {
+        return true;
+    }
+
+    switch (keycode) {
+        // case MCTL_ESC: {
+        //     CTL_T(KC_ESC)
+        //     break;
+        // }
+    }
+    return true;
+};
+
 bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
     // add layer indicators
     switch(get_highest_layer(layer_state|default_layer_state)) {
@@ -33,7 +64,7 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
 
     // Add indicator if caps_lock or caps_word is on
     if (host_keyboard_led_state().caps_lock || is_caps_word_on()) {
-        RGB_MATRIX_INDICATOR_SET_COLOR(25, 255, 0, 0); // assuming caps lock is at led #5
+        RGB_MATRIX_INDICATOR_SET_COLOR(25, 255, 0, 0);
     } else {
         RGB_MATRIX_INDICATOR_SET_COLOR(25, 0, 0, 0);
     }
@@ -48,13 +79,11 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
 };
 #endif
 
-#define MCTL_ESC CTL_T(KC_ESC)
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_65_ansi_blocker(
     KC_GRV,     KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       KC_MINS,    KC_EQL,     KC_BSPC,    KC_MUTE,
     KC_TAB,     KC_Q,       KC_W,       KC_E,       KC_R,       KC_T,       KC_Y,       KC_U,       KC_I,       KC_O,       KC_P,       KC_LBRC,    KC_RBRC,    KC_BSLS,    KC_DELETE,
-    MCTL_ESC,   KC_A,       KC_S,       KC_D,       KC_F,       KC_G,       KC_H,       KC_J,       KC_K,       KC_L,       KC_SCLN,    KC_QUOT,                KC_ENT,     KC_PAGE_UP,
+    CTL_ESC,   KC_A,       KC_S,       KC_D,       KC_F,       KC_G,       KC_H,       KC_J,       KC_K,       KC_L,       KC_SCLN,    KC_QUOT,                KC_ENT,     KC_PAGE_UP,
     KC_LSFT,    KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_N,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    KC_RSFT,                KC_UP,      KC_PAGE_DOWN,
     MO(1),      KC_LGUI,    KC_LALT,                            KC_SPC,                             MO(2),      KC_RCTL,                            KC_LEFT,    KC_DOWN,    KC_RIGHT
     ),
